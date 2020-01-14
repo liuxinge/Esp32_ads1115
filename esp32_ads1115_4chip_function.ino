8d82e4e5a6decc0ee330339e1bc100aa565928ab
Luke Liu <liuxinge@gmail.com>
2020-01-14 16:11:49 -0600

    new

---
+++ esp32_ads1115_4chip_function.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+// This is an funciton for Esp32 to connect 4 ads1115 chips and read each
+// chip to get each channel's input.
+
+void setup(void) 
+{
+  Serial.begin(9600);
+}
+
+void loop(void){
+  ADS1115();
+}
+
+void ADS1115(void) 
+{
+ 
+ double AinV[16];
+ double gainFactor = 0.0001875;
+ int I2C_address = 0x48;
+ 
+ for (int j = 0; j < 4; j++){
+
+  I2C_address = I2C_address + j;
+  Adafruit_ADS1115 ads(I2C_address);  /* Use this for the 16-bit version */
+  ads.begin();
+
+  int16_t adcRaw;
+  
+  for(int i = 0; i < 4; i ++){ 
+     adcRaw = ads.readADC_SingleEnded(i); 
+     if (adcRaw < 0) {adcRaw = 0;}
+     AinV[i+j*4] = gainFactor * adcRaw;
+     Serial.print("AinV"); Serial.print(i+j*4); Serial.print(": ");Serial.println(AinV[i+j*4], 4);
+
+    } 
+  Serial.println("");
+ }
+  delay(1000);
+}
